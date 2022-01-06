c8643a1a14f5248122a3fb17dcb1c1de0e2f2e02
Dmitry <dmitry.galay.91@gmail.com>
2022-01-06 12:30:56 +0300

    Create project

---
+++ Test_App/Test_App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Test_App
+//
+//  Created by Dima on 6.01.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
