8cb81d58739bd3b7d15329eff25290c5d6160274
Dmitry <dmitry.galay.91@gmail.com>
2022-01-15 22:13:08 +0300

    update

--- SwiftUIProjects/SwiftUIProjects/SwiftUIProjects/ContentView.swift
+++ SwiftUIProjects/SwiftUIProjects/SwiftUIProjects/ContentView.swift
@@ -8,19 +8,189 @@
 import SwiftUI
 
 struct ContentView: View {
+    let lenght: CGFloat = 100
     var body: some View {
+//        Color.gray
+//               .overlay(
+        
         VStack {
-            Rectangle()
-                .fill(.red)
-                .frame(width: 400, height: 400, alignment: .top)
-            Rectangle()
-                .fill(.red)
-                .frame(width: 400, height: 100, alignment: .top)
-            Rectangle()
-                .fill(.red)
-                .frame(width: 100, height: 100, alignment: .top)
+            VStack {
+                VStack {
+                    HStack {
+                        Image("sunny").frame(width: lenght, height: lenght)
+                        VStack {
+                            Text("13º")
+                                .font(.largeTitle)
+                                .fontWeight(.bold)
+                                .foregroundColor(Color.black)
+                                .multilineTextAlignment(.center)
+                                .frame(width: lenght, height: 30, alignment: .center)
+                            
+                            Text("Sunny")
+                                .font(.body)
+                                .fontWeight(.bold)
+                                .foregroundColor(Color.orange)
+                                .frame(width: lenght, height: 30, alignment: .center)
+                        }
+                        
+                    }.padding(.leading, lenght / 2 )
+                        .frame(width: lenght * 4, height: lenght * 2, alignment: .leading)
+                    
+                    VStack {
+                        HStack {
+                            Spacer().frame(width: lenght / 2 )
+                            Image("sunnySmall")
+                            Text("Friday")
+                                .font(.body)
+                                .fontWeight(.medium)
+                                .foregroundColor(Color.black)
+                                .frame(width: lenght, height: 30, alignment: .leading)
+                            Spacer()
+                            Text("12º")
+                                .fontWeight(.bold)
+                                .foregroundColor(Color.black)
+                                .frame(width: lenght, height: 30, alignment: .leading)
+                        }
+                        
+                        HStack {
+                            Spacer().frame(width: lenght / 2 )
+                            Image("cloudySmall")
+                            Text("Saturday")
+                                .font(.body)
+                                .fontWeight(.medium)
+                                .foregroundColor(Color.black)
+                                .frame(width: lenght, height: 30, alignment: .leading)
+                            Spacer()
+                            Text("14º")
+                                .fontWeight(.bold)
+                                .foregroundColor(Color.black)
+                                .frame(width: lenght, height: 30, alignment: .leading)
+                        }
+                        HStack {
+                            Spacer().frame(width: lenght / 2 )
+                            Image("rainySmall")
+                            Text("Sunday")
+                                .font(.body)
+                                .fontWeight(.medium)
+                                .foregroundColor(Color.black)
+                                .frame(width: lenght, height: 30, alignment: .leading)
+                            Spacer()
+                            Text("17º")
+                                .fontWeight(.bold)
+                                .foregroundColor(Color.black)
+                                .frame(width: lenght, height: 30, alignment: .leading)
+                        }
+                        
+                    }
+                    .frame(width: lenght * 4, height: lenght * 2, alignment: .center)
+                    
+                }.overlay(
+                    RoundedRectangle(cornerRadius: 20)
+                        .stroke(Color.black, lineWidth: 1)
+                        .frame(width: lenght * 4, height: lenght * 4, alignment: .top)
+                )
+            }
+            
+            .frame(width: 400, height: 400, alignment: .top)
+            Spacer().frame(height: 75)
+                .foregroundColor(Color.red)
+            HStack {
+                Image("sunnyAverage")
+                VStack {
+                    Text("13º")
+                        .font(.largeTitle)
+                        .fontWeight(.bold)
+                        .foregroundColor(Color.black)
+                        .multilineTextAlignment(.center)
+                        .frame(width: lenght, height: 30, alignment: .center)
+                    
+                    Text("Sunny")
+                        .font(.headline)
+                        .fontWeight(.bold)
+                        .foregroundColor(Color.orange)
+                        .frame(width: lenght, height: 30, alignment: .center)
+                }
+                VStack {
+                    HStack {
+                        
+                        Image("sunnySmall")
+                        Text("Fri")
+                            .font(.body)
+                            .fontWeight(.medium)
+                            .foregroundColor(Color.black)
+                            .frame(width: lenght / 2, height: 30, alignment: .leading)
+                        Text("12º")
+                            .fontWeight(.bold)
+                            .foregroundColor(Color.black)
+                            .frame(width: lenght / 2, height: 30, alignment: .leading)
+                    }
+                    
+                    HStack {
+                        
+                        Image("cloudySmall")
+                        Text("Sat")
+                            .font(.body)
+                            .fontWeight(.medium)
+                            .foregroundColor(Color.black)
+                            .frame(width: lenght / 2, height: 30, alignment: .leading)
+                        
+                        Text("14º")
+                            .fontWeight(.bold)
+                            .foregroundColor(Color.black)
+                            .frame(width: lenght / 2, height: 30, alignment: .leading)
+                    }
+                    HStack {
+                        
+                        Image("rainySmall")
+                        Text("Sun")
+                            .font(.body)
+                            .fontWeight(.medium)
+                            .foregroundColor(Color.black)
+                            .frame(width: lenght / 2, height: 30, alignment: .leading)
+                        
+                        Text("17º")
+                            .fontWeight(.bold)
+                            .foregroundColor(Color.black)
+                            .frame(width: lenght / 2, height: 30, alignment: .leading)
+                    }
+                    
+                }
+                
+            }
+            .overlay(
+                RoundedRectangle(cornerRadius: 20)
+                    .stroke(Color.black, lineWidth: 1)
+                    .frame(width: lenght * 4, height: lenght * 2, alignment: .center)
+            )
+            Spacer().frame(height: 100)
+            VStack {
+                Image("sunnyAverage")
+                VStack {
+                    Text("13º")
+                        .font(.largeTitle)
+                        .fontWeight(.bold)
+                        .foregroundColor(Color.black)
+                        .multilineTextAlignment(.center)
+                        .frame(width: lenght, height: 30, alignment: .center)
+                    
+                    Text("Sunny")
+                        .font(.body)
+                        .fontWeight(.bold)
+                        .foregroundColor(Color.orange)
+                        .frame(width: lenght, height: 30, alignment: .center)
+                }
+                
+            }
+            .overlay(
+                RoundedRectangle(cornerRadius: 20)
+                    .stroke(Color.black, lineWidth: 1)
+                    .frame(width: lenght * 2 , height: lenght  * 2, alignment: .center)
+            )
+            
         }
+//        )
     }
+        
 }
 
 struct ContentView_Previews: PreviewProvider {
@@ -28,3 +198,4 @@ struct ContentView_Previews: PreviewProvider {
         ContentView()
     }
 }
+
